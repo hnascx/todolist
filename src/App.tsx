c25e3dda02e6cdeeb33a372b5f8fa526f3618aaa
hnascx <henriquehusky@gmail.com>
2023-05-23 14:51:18 -0300

    NewTask component style done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Header from './components/Header'
+import NewTask from './components/NewTask'
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <NewTask />
+      </div>
     </div>
   )
 }
---
+++ src/components/NewTask.tsx
@@ -0,0 +1,21 @@
+import styles from './NewTask.module.css'
+import addTaskLogo from '../assets/addtask-img.svg'
+
+/* Mudar a estrutura e utilizar section com input e button invés da estrutura abaixo? */
+export function NewTask() {
+  return (
+    <form className={styles.newtask}>
+    <textarea
+      placeholder='Adicione uma nova tarefa'
+    />
+    <footer>
+      <button>
+        Criar
+        <img src={addTaskLogo} alt='Adicionar tarefa' />
+      </button>
+    </footer>
+  </form>
+  )
+}
+
+export default NewTask
